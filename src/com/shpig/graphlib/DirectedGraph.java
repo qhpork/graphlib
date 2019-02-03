2b7eeb1a4c16fe70f8025d7f6447f0a9e21a9ed5
shpig <47092549+shpig@users.noreply.github.com>
2019-02-03 16:23:46 +1100

    javadocs sucks (i fixed it)

--- src/com/shpig/graphlib/DirectedGraph.java
+++ src/com/shpig/graphlib/DirectedGraph.java
@@ -5,21 +5,24 @@ package com.shpig.graphlib;
  */
 public interface DirectedGraph<N extends Comparable<N>, T> {
 
-    /**Add a vertex with name and value to the graph, no edges will be connected to this vertex
+    /**
+     * Add a vertex with name and value to the graph, no edges will be connected to this vertex
      * 
      * @param label	
      * @param value 
      */
     void addVertex(N label, T value);
     
-    /**Remove a vertex given its name, the vertex is returned
+    /**
+     * Remove a vertex given its name, the vertex is returned
      * 
      * @param label
      * @return
      */
     T removeVertex(N label);
     
-    /**Add an edge between two pre-existing vertices
+    /**
+     * Add an edge between two pre-existing vertices
      * 
      * @param start
      * @param end
@@ -27,21 +30,24 @@ public interface DirectedGraph<N extends Comparable<N>, T> {
      */
     void addEdge(N start, N end, int weight);
     
-    /**Find a specific vertex given its name, the vertex is returned
+    /**
+     * Find a specific vertex given its name, the vertex is returned
      * 
      * @param label
      * @return
      */
     Node<N, T> getVertex(N label);
     
-    /**Get the value of a vertex given its name
+    /**
+     * Get the value of a vertex given its name
      * 
      * @param label
      * @return
      */
     T getVertexValue(N label);
     
-    /**Set the value of a vertex
+    /**
+     * Set the value of a vertex
      * 
      * @param label
      * @param value
@@ -49,14 +55,16 @@ public interface DirectedGraph<N extends Comparable<N>, T> {
      */
     T setVertexValue(N label, T value);
 
-    /**Get the value of a edge given the two contacting vertices
+    /**
+     * Get the value of a edge given the two contacting vertices
      * 
      * @param start
      * @param end
      */
     void getEdgeValue(N start, N end);
     
-    /**Set the value of a edge given the two contacting vertices
+    /**
+     * Set the value of a edge given the two contacting vertices
      * @param start
      * @param end
      * @return the previous value is returned
@@ -64,7 +72,8 @@ public interface DirectedGraph<N extends Comparable<N>, T> {
     int setEdgeValue(N start, N end);
 
     
-    /**Default implementation of {@link #addEdge(Comparable, Comparable, int)}, weight is default to 0
+    /**
+     * Add an edge between two pre-existing vertices, weight is default to 0
      * 
      * @param start
      * @param end
